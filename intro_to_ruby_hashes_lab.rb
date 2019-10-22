497539ff1610fa8268df265743439e6c0e7a3a66
Kizito Njoku <nkizito57@gmail.com>
2019-10-22 18:15:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,37 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+<<<<<<< HEAD
 monopoly = {
 	  :railroads => {}
 	 }
 	 monopoly
+=======
+hash{
+	  :african => {
+	    "names" => {
+	      :first => "kizito",
+	      :last => "njoku"
+	     },
+	  "cars" => {
+	     :toyota => "camry",
+	     :honda => "accord"
+	  }
+	  },
+	  :eletronics => {
+	     "phones" => {
+	       :samsung => "galaxy",
+	       :apple => "ipon x"
+	       
+	     },
+	       "Tablets" => {
+	         :sam => "sn2",
+	         :ipad => "mini"
+	       }
+	  }
+	 }
+	 hash
+>>>>>>> 787a7752704b3c4d29dd6abbcc8e3e70a94a1d0d
 end
 
 def monopoly_with_second_tier
@@ -12,6 +39,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+<<<<<<< HEAD
   monopoly = {
 	  :railroads => {
 	  :rent_in_dollars => {},
@@ -20,6 +48,32 @@ def monopoly_with_second_tier
 	 }
   }
 	 
+=======
+  hash{
+	  :african => {
+	    "names" => {
+	      :first => "kizito",
+	      :last => "njoku"
+	     },
+	  "cars" => {
+	     :toyota => "camry",
+	     :honda => "accord"
+	  }
+	  },
+	  :eletronics => {
+	     "phones" => {
+	       :samsung => "galaxy",
+	       :apple => "ipon x"
+	       
+	     },
+	       "Tablets" => {
+	         :sam => "sn2",
+	         :ipad => "mini"
+	       }
+	  }
+	 }
+	 hash
+>>>>>>> 787a7752704b3c4d29dd6abbcc8e3e70a94a1d0d
 end
 
 def monopoly_with_third_tier
